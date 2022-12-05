c3ffa0c93fecc8818443f721bac8ce1d01f80da1
cool-Moshood <moshoodage@gmail.com>
2022-12-05 07:50:05 +0100

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if my_string[i] != "c" and my_string[i] != "C":
             new_string.append(my_string[i])
-    return"".join(new_string):
+    return"".join(new_string)
