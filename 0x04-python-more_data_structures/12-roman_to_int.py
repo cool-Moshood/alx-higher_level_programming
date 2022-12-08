5348f952026eac44fea4d6e3b88b1fccff16b950
cool-Moshood <moshoodage@gmail.com>
2022-12-08 03:06:27 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,6 +7,8 @@ roman = __import__('roman').roman
 def roman_to_int(roman_string):
     x = 0
     y = 0
+    if not roman_string or type(roman_string) != str:
+        return 0
     while y < len(roman_string):
         s1 = roman(roman_string[y])
         if y + 1 < len(roman_string):
