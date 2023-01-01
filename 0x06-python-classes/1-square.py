8c5532558871f6938ef6ea7fec478afd900f67a7
cool-Moshood <moshoodage@gmail.com>
2023-01-01 14:33:55 +0100

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,13 @@
-ht
+#!/usr/bin/python3
+"""
+_summary_
+"""
+
+
+class Square:
+    """Square class
+    private instance field : name
+    """
+
+    def __init__(self, size):
+        self.__size = size
