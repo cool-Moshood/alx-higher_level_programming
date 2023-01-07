96d1a1ca278719d2e6ffa0af925f0204c3440e04
cool-Moshood <moshoodage@gmail.com>
2023-01-07 22:49:18 +0100

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+ a class Rectangle that defines a rectangle
+ """
+
+
+class Rectangle:
+    """ Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """initializes the class
+
+        Args:
+            width(int, optional): width. Defaulths to 0.
+            height (int, optional): height. Defaults to 0.
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """width getter
+
+        Return:
+            int: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter
+
+        Args:
+            value (int): new width
+
+        Raises:
+            ValueError: width must be >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter
+
+        Return:
+            int: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter
+
+        Args:
+            value (int): height
+
+        Raises:
+            ValueError: height must be >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
