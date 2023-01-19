0bf5b45e37a4e2375298b589fd753341fc9c9403
cool-Moshood <moshoodage@gmail.com>
2023-01-19 18:52:28 +0100

    hot fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,63 +1,71 @@
 #!/usr/bin/python3
-"""unit test module to for max_integer function"""
+"""Unittest for max_integer([..])
+"""
 import unittest
-
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Test class
-    Args:
-        unittest (class): inherits from unittest
-    """
-
+    """unittest class for max_integer"""
     def test_module_docstring(self):
-        """Tests module for docstring
-        """
-        m = ('6-max_integer').__doc__
+        """Tests for module docsting"""
+        m = __import__('6-max_integer').__doc__
         self.assertTrue(len(m) > 1)
 
     def test_function_docstring(self):
-        """Test function has a docstring
-        """
-        m = max_integer.__doc__
-        self.assertTrue(len(m) > 1)
+        """Tests for funstion docstring"""
+        f = max_integer.__doc__
+        self.assertTrue(len(f) > 1)
 
-    def test_max_integer(self):
-        """correct test
-        """
-        result = max_integer([1, 4, 3, 2])
-        self.assertEqual(4, result)
+    def test_empty_list(self):
+        """Tests for empty list []"""
+        e = []
+        self.assertIsNone(max_integer(e))
 
-    def test_two_max_integers(self):
-        """correct test
-        """
-        result = max_integer([1, 4, 5, 3, 2, 5])
-        self.assertEqual(5, result)
+    def test_no_args(self):
+        """Tests for no arguments passed to func"""
+        self.assertIsNone(max_integer())
 
-    def test_list_is_empty(self):
-        """List is empty test
-        """
-        result = max_integer([])
-        self.assertEqual(None, result)
+    def test_one_element(self):
+        """Tests for only one number in the list"""
+        o = [1]
+        self.assertEqual(max_integer(o), 1)
 
-    def test_list_param_contains_only_one_arg(self):
-        """List has more than 1 param
-        """
-        self.assertRaises(TypeError,
-                          max_integer, 1, "2")
+    def test_positive_end(self):
+        """Tests for all positive with max at end"""
+        e = [2, 10, 8, 36, 14, 50]
+        self.assertEqual(max_integer(e), 50)
 
-    def test_list_param_contains_only_one_arg(self):
-        """List has more than 1 param
-        """
-        self.assertRaises(TypeError,
-                          max_integer, [1, 2], [3, 4])
+    def test_positive_middle(self):
+        """Tests for all positive with max in middle"""
+        m = [2, 10, 8, 360, 14, 50]
+        self.assertEqual(max_integer(m), 360)
 
-    def test_not_supported(self):
-        """List param not supported
-        """
-        self.assertRaisesRegex(TypeError, "object of type *", max_integer, 1)
+    def test_positive_beginning(self):
+        """Tests for all positive with max at beginning"""
+        b = [200, 10, 8, 36, 14, 50]
+        self.assertEqual(max_integer(b), 200)
 
+    def test_one_negative(self):
+        """Tests for list with one negative number"""
+        on = [200, 10, 8, -36, 14, 50]
+        self.assertEqual(max_integer(on), 200)
 
-if __name__ == '__main__':
+    def test_all_negative(self):
+        """Tests for list with all negative numbers"""
+        n = [-6, -50, -75, -1, -100]
+        self.assertEqual(max_integer(n), -1)
+
+    def test_none(self):
+        """Tests for passing none as argument"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_non_int_arg(self):
+        """Tests for a non-int type in list"""
+        string = [1, 2, "Hello", 4, 5]
+        with self.assertRaises(TypeError):
+            max_integer(string)
+
+if __name__ == "__main__":
     unittest.main()
