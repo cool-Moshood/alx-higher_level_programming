9aac679fc56ac005829444017bacbc9c04748f20
cool-Moshood <moshoodage@gmail.com>
2023-01-22 22:07:05 +0100

    task

---
+++ 0x07-python-test_driven_development/test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""function that divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+
+    r = []
+    j = len(matrix[0])
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    for row in matrix:
+        if len(row) != j:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for row in matrix:
+        for ele in row:
+            if type(ele) is not int and type(ele) is not float:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists)"
+                    " of integers/floats")
+
+    r = [[round(x / div, 2) for x in row] for row in matrix]
+    return r
+
+
+try:
+    matrix = [[3, "9"], [12, 3]]
+    print(matrix_divided(matrix, 2))
+    print(matrix)
+except Exception as e:
+    print(e)
+
+try:
+    matrix = [[3, 9], [12, 3]]
+    print(matrix_divided(matrix, "2"))
+    print(matrix)
+except Exception as e:
+    print(e)
