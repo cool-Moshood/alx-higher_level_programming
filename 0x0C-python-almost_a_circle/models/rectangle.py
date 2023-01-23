12b36991fd771571f0fe204fe2db3202b1fb01f4
cool-Moshood <moshoodage@gmail.com>
2023-01-24 00:27:56 +0100

    task 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -123,3 +123,9 @@ class Rectangle(Base):
 
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Rectangle"""
+        return {"id": self.id, "width": self.width,
+            "height": self.height, "x": self.x,
+             "y": self.y}
