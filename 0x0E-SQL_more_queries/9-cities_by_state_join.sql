898c266f0bacb0b02588e9b10576404920ff7185
cool-Moshood <moshoodage@gmail.com>
2023-02-19 22:13:44 +0100

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
-SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY ASC;
+SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
