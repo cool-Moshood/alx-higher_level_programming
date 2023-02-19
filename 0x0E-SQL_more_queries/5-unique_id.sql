391ce4812c33d83030075589854ec805375d280f
cool-Moshood <moshoodage@gmail.com>
2023-02-19 20:02:28 +0100

    task 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- a script that creates the table unique_id on your MySQL server.
 -- unique_id description: id INT with the default value 1 and must be unique, name VARCHAR(256)
 -- If the table unique_id already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS unique_id(id INT NOT NULL DEFAULT '1', name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT NOT NULL DEFAULT '1' PRIMARY KEY, name VARCHAR(256));
