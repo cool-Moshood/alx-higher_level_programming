13433a7c82c10f7919fd0c5c8773cc8f9c37abe0
cool-Moshood <moshoodage@gmail.com>
2023-02-19 11:56:54 +0100

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- a script that create the mysql server user
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost'
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
