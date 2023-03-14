9ffb26a778fc8c0c9cd89b5f9d3be1317b64a0e8
cool-Moshood <moshoodage@gmail.com>
2023-03-14 11:42:16 +0100

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
-let x = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	let y = 0;
-	while (y < x) {
-		console.log('C is fun');
-		y++;
-	}
+  let y = 0;
+  while (y < x) {
+    console.log('C is fun');
+    y++;
+  }
 }
-
