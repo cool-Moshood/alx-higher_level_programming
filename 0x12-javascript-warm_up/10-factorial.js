cc867e6641f5e2950e602200abd2a530101eedd4
cool-Moshood <moshoodage@gmail.com>
2023-03-14 04:40:33 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,11 +4,11 @@ let x = parseInt(process.argv[2]);
 
 function factorial(x) {
 
-    if (x === 1 || isNaN(x)) {
-        return 1;
-    } else {
-        return x * factorial(x - 1);
-    }
+	if (x === 1 || isNaN(x)) {
+		return 1;
+	} else {
+		return x * factorial(x - 1);
+	}
 }
 
 let y = 1;
