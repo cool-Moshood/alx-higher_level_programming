8b10487e56549d642184df083c5afef86d1a4366
cool-Moshood <moshoodage@gmail.com>
2023-03-14 11:42:45 +0100

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,14 +3,13 @@
 const x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	for (let j = 0; j < x; j++) {
-		let y = "";
-		for (let i = 0; i < x; i++) {
-			y += 'X';
-		}
-		console.log(y);
-	}
+  for (let j = 0; j < x; j++) {
+    let y = '';
+    for (let i = 0; i < x; i++) {
+      y += 'X';
+    }
+    console.log(y);
+  }
 }
-
