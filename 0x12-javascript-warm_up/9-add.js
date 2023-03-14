2b283969893e12cb6c6221f2e1e1319725ca07c9
cool-Moshood <moshoodage@gmail.com>
2023-03-14 04:54:22 +0100

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,9 +4,10 @@ let first = parseInt(process.argv[2]);
 let second = parseInt(process.argv[3]);
 
 function add(a, b) {
-    return (a + b);
+	return (a + b);
 }
 
 let total = 0;
 total = add(first, second);
 console.log(total);
+
