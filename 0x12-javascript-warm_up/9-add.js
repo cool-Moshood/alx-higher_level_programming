7aa5e0d9d6117ca44967612974c770de5e66d962
cool-Moshood <moshoodage@gmail.com>
2023-03-14 02:16:14 +0100

    task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let first = parseInt(process.argv[2]);
+let second = parseInt(process.argv[3]);
+
+function add(a, b) {
+    return (a + b);
+}
+
+let total = 0;
+total = add(first, second);
+console.log(total);
