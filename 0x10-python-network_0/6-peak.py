df3ee230e17bbef387f2df106912fe34633b2739
cool-Moshood <moshoodage@gmail.com>
2023-04-05 04:16:03 +0100

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,9 +12,9 @@ def find_peak(list_of_integers):
         return
     mid_index = size // 2
     if (mid_index == size - 1 or nums[mid_index] >= nums[mid_index + 1]) \
-            and (nums[mid_index] >= nums[mid_index -1] or mid_index == 0):
+            and (nums[mid_index] >= nums[mid_index - 1] or mid_index == 0):
         return nums[mid_index]
-    if (mid_index != size -1) and nums[mid_index + 1] > nums[mid_index]:
+    if (mid_index != size - 1) and nums[mid_index + 1] > nums[mid_index]:
         return find_peak(nums[mid_index + 1:])
     else:
         return find_peak(nums[:mid_index])
