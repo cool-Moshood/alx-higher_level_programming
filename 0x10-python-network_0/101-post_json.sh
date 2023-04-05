12a15c93dc22caf3c2d500a38c3f67e314e697ad
cool-Moshood <moshoodage@gmail.com>
2023-04-05 04:22:18 +0100

    task 8

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
